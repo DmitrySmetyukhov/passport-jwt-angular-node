c66372e1a9cacc34e37dc0d31f0cb855d2f4cb25
Dmitry <dmitry.smetyukhov@gmail.com>
2023-11-23 13:46:02 +0200

    view engine

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const routes = require('./routes');
 const passport = require('passport');
 require('./config/database.js');
 require('./config/passport')(passport);
-
 require('dotenv').config();
+
+
 const app = express();
 
-// require('./config/passport')(passport);
 app.use(passport.initialize());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(routes);
 
 app.listen(3000, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 //TODO
 router.get('/register', (req, res, next) => {
-   res.send('REGISTER')
+   res.render('register')
 });
 
 //TODO
