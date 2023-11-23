359fbfdd0d579267f41e4551a602f9651f1d8048
Dmitry <dmitry.smetyukhov@gmail.com>
2023-11-23 13:59:42 +0200

    register basic routes & form

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 //TODO
 router.get('/register', (req, res, next) => {
-   res.render('register')
+    res.render('register')
 });
+
+router.post('/register', (req, res) => {
+    console.log(req.body)
+})
 
 //TODO
 router.get('/login', (req, res, next) => {
