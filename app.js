c3d20c2724e6aaa7bbaa32b67a099f9f708154d1
Dmitry <dmitry.smetyukhov@gmail.com>
2023-11-23 13:37:35 +0200

    app.js basic middlewares

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 const passport = require('passport');
 require('./config/database.js');
+require('./config/passport')(passport);
 
 require('dotenv').config();
 const app = express();
 
-require('./config/passport')(passport);
-
+// require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 app.listen(3000, () => {
