c2ccecca277fa33a7064f1dd0f70937527deebd5
Dmitry <dmitry.smetyukhov@gmail.com>
2023-11-23 12:59:04 +0200

    passport config file

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const User = require('../models/user');
+
+const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
+const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+
+//TODO 
+const options = {};
+
+module.exports = (passport) => { }
